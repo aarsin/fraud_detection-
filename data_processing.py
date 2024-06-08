40fc86228dd9b82efe718acce7c93d4c842df655
aarsin <singh.aara2913@gmail.com>
2024-06-08 19:33:42 +0530

    corrected the code for sum

--- data_processing.py
+++ data_processing.py
@@ -1,10 +1,15 @@
 print("adding all the data processing code related for fraud detetion")
+
 a=5
 b=7
 
-def sum(a,b);
-    c= a+b
+def sum(a,b):
+    c=a+b
     return c
+print("sum function")
 
+def multiplication(a,b):
+    d = a*b
+    return b
 
-print("added a new function sum which is not correct")
+print("multiplication function")
