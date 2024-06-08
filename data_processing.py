02d9aff7d736fabb8e96dbda62e403eaf9969f6a
aarsin <singh.aara2913@gmail.com>
2024-06-08 19:24:15 +0530

    added a new code

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,10 @@
 print("adding all the data processing code related for fraud detetion")
+a=5
+b=7
+
+def sum(a,b);
+    c= a+b
+    return c
+
+
+print("added a new function sum which is not correct")
